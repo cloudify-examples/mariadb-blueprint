2e2747aaa79cce996fc235cac8b0014156d562b1
earthmant <trammell@cloudify.co>
2017-09-26 13:44:17 +0300

    supporting Galera Cluster

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+from cloudify import ctx
 from cloudify.state import ctx_parameters as inputs
 
 try:
@@ -15,12 +16,34 @@ if __name__ == '__main__':
 
     old_password = inputs.get('old_password', str())
     new_password = inputs.get('new_password', old_password)
+    users = inputs.get('new_users', {})
 
     db = mariadb.connect(user='root', passwd=old_password, db='mysql')
     cur = db.cursor()
+
     cur.execute("UPDATE mysql.user SET Password = PASSWORD('{0}') WHERE User = 'root'".format(new_password))
     cur.execute("DROP USER ''@'localhost'")
     cur.execute("DROP USER ''@'{0}'".format(os.uname()[1]))
     cur.execute("DROP DATABASE test")
+
+    for username, userdescription in users.items():
+
+        create_user = "CREATE USER '{0}'@'{1}' IDENTIFIED BY '{2}';".format(
+            username,
+            userdescription.get('host', '%'),
+            userdescription.get('password', new_password))
+
+        create_user_grant = "GRANT {0} ON {1} TO '{2}'@'{3}' WITH GRANT OPTION;".format(
+            'ALL PRIVILEGES' if userdescription.get('privileges')[0] == 'ALL PRIVILEGES' else ', '.join(userdescription.get('privileges')),
+            userdescription.get('database', '*.*'),
+            username,
+            userdescription.get('host', '%'))
+
+        ctx.logger.debug(create_user)
+        ctx.logger.debug(create_user_grant)
+
+        cur.execute(create_user)
+        cur.execute(create_user_grant)
+
     cur.execute("FLUSH PRIVILEGES")
     db.close()
--- scripts/start.py
+++ scripts/start.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import subprocess
+import tempfile
 from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
 from cloudify.exceptions import OperationRetry
 
 
-def execute_command(_command):
+def execute_command(_command, env_vars=None):
 
     ctx.logger.debug('_command {0}.'.format(_command))
 
@@ -14,6 +16,10 @@ def execute_command(_command):
         'stdout': subprocess.PIPE,
         'stderr': subprocess.PIPE
     }
+
+    if env_vars:
+        # env_vars = execute_command('printenv')
+        subprocess_args['env'] = env_vars
 
     ctx.logger.debug('subprocess_args {0}.'.format(subprocess_args))
 
@@ -33,17 +39,48 @@ def execute_command(_command):
 
 if __name__ == '__main__':
 
-wsrep_on=ON
-wsrep_provider=/usr/lib64/galera/libgalera_smm.so
-wsrep_cluster_address='gcomm://10.0.0.11,10.0.0.12,10.0.0.13'
-wsrep_cluster_name='cluster1.tunts'
-wsrep_node_address='10.0.0.11'
-wsrep_node_name='gal1'
-wsrep_sst_method=rsync
-binlog_format=row
-default_storage_engine=InnoDB
-innodb_autoinc_lock_mode=2
-#first node
-galera_new_cluster
-#other nodes
-systemctl start mariadb
+    execute_command('sudo systemctl stop mariadb')
+    cluster_addresses = inputs.get('cluster_addresses', [])
+    master = inputs.get('master', ctx.instance.host_ip)
+
+    if ctx.instance.host_ip not in cluster_addresses:
+        cluster_addresses.append(ctx.instance.host_ip)
+
+    config = """[galera]
+wsrep_on={0}
+binlog_format={1}
+default-storage-engine={2}
+innodb_autoinc_lock_mode={3}
+wsrep_provider={4}
+wsrep_cluster_name='{5}'
+wsrep_node_address='{6}'
+wsrep_node_name='{7}'
+wsrep_sst_method={8}
+wsrep_cluster_address='{9}'
+""".format(
+        inputs.get('wsrep_on', 'ON'),
+        inputs.get('binlog_format', 'row'),
+        inputs.get('default_storage_engine', 'InnoDB'),
+        inputs.get('innodb_autoinc_lock_mode', '2'),
+        inputs.get('wsrep_provider', '/usr/lib64/galera/libgalera_smm.so'),
+        ctx.deployment.id,
+        ctx.instance.host_ip,
+        ctx.instance.id,
+        inputs.get('wsrep_sst_method', 'rsync'),
+        inputs.get('wsrep_cluster_address',
+                   'gcomm://{0}'.format(','.join(cluster_addresses)))
+    )
+
+    tf = tempfile.NamedTemporaryFile()
+    with open(tf.name, 'w') as outfile:
+        outfile.write(config)
+
+    execute_command('sudo cp {0} {1}'.format(tf.name, '/etc/my.cnf.d/server.cnf'))
+
+    if ctx.workflow_id == 'install' and master == ctx.instance.host_ip:
+        started = execute_command('sudo /usr/bin/galera_new_cluster')
+    else:
+        started = execute_command('sudo systemctl start mariadb')
+
+    if started is False:
+        raise OperationRetry('Didnt start/add cluster node. Retrying.')
