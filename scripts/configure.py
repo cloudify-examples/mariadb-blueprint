7ad0ac406d0b3173c9cfc41ac21de6c83c6bc9c8
earthmant <trammell@cloudify.co>
2018-01-17 10:32:05 +0200

    Solutions Package Changes

--- scripts/configure.py
+++ scripts/configure.py
@@ -14,36 +14,42 @@ except ImportError:
 
 if __name__ == '__main__':
 
-    old_password = inputs.get('old_password', str())
-    new_password = inputs.get('new_password', old_password)
-    users = inputs.get('new_users', {})
+    db_password = inputs.get('db_password', str())
+    mysql_commands = inputs.get('mysql_commands', [])
+    # new_password = inputs.get('new_password', old_password)
+    # users = inputs.get('new_users', {})
 
-    db = mariadb.connect(user='root', passwd=old_password, db='mysql')
+    db = mariadb.connect(user='root', passwd=db_password, db='mysql')
     cur = db.cursor()
 
-    cur.execute("UPDATE mysql.user SET Password = PASSWORD('{0}') WHERE User = 'root'".format(new_password))
-    cur.execute("DROP USER ''@'localhost'")
-    cur.execute("DROP USER ''@'{0}'".format(os.uname()[1]))
-    cur.execute("DROP DATABASE test")
+    # if new_password != old_password:
+    #     cur.execute("UPDATE mysql.user SET Password = PASSWORD('{0}') WHERE User = 'root'".format(new_password))
+    #     cur.execute("DROP USER ''@'localhost'")
+    #     cur.execute("DROP USER ''@'{0}'".format(os.uname()[1]))
+    #     cur.execute("DROP DATABASE test")
 
-    for username, userdescription in users.items():
+    for mysql_command in mysql_commands:
+        ctx.logger.debug('COMMAND: {0}'.format(mysql_command))
+        cur.execute(mysql_command)
 
-        create_user = "CREATE USER '{0}'@'{1}' IDENTIFIED BY '{2}';".format(
-            username,
-            userdescription.get('host', '%'),
-            userdescription.get('password', new_password))
+    # for username, userdescription in users.items():
 
-        create_user_grant = "GRANT {0} ON {1} TO '{2}'@'{3}' WITH GRANT OPTION;".format(
-            'ALL PRIVILEGES' if userdescription.get('privileges')[0] == 'ALL PRIVILEGES' else ', '.join(userdescription.get('privileges')),
-            userdescription.get('database', '*.*'),
-            username,
-            userdescription.get('host', '%'))
+    #     create_user = "CREATE USER '{0}'@'{1}' IDENTIFIED BY '{2}';".format(
+    #         username,
+    #         userdescription.get('host', '%'),
+    #         userdescription.get('password', new_password))
 
-        ctx.logger.debug(create_user)
-        ctx.logger.debug(create_user_grant)
+    #     create_user_grant = "GRANT {0} ON {1} TO '{2}'@'{3}' WITH GRANT OPTION;".format(
+    #         'ALL PRIVILEGES' if userdescription.get('privileges')[0] == 'ALL PRIVILEGES' else ', '.join(userdescription.get('privileges')),
+    #         userdescription.get('database', '*.*'),
+    #         username,
+    #         userdescription.get('host', '%'))
 
-        cur.execute(create_user)
-        cur.execute(create_user_grant)
+    #     ctx.logger.debug(create_user)
+    #     ctx.logger.debug(create_user_grant)
 
-    cur.execute("FLUSH PRIVILEGES")
+    #     cur.execute(create_user)
+    #     cur.execute(create_user_grant)
+
+    # cur.execute("FLUSH PRIVILEGES")
     db.close()
